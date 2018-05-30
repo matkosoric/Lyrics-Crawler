9e81c8c2641cc19931d954704e72b159be4b438a
Matko-ETK <soric.matko@gmail.com>
2018-05-30 09:43:59 +0200

    crawling success

--- src/main/java/LyricsCrawler.java
+++ src/main/java/LyricsCrawler.java
@@ -8,6 +8,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,26 +19,51 @@ public class LyricsCrawler {
 
     public static void main(String[] args) {
 
-        Document doc = null;
+        List<String> songLinks = scrapeSongLinks("https://tekstovi.net/2,182,0.html");
 
+        songLinks.forEach(LyricsCrawler::extractLyrics);
+
+    }
+
+
+
+    // skupljanje linkova na pjesme po izvođaču
+    public static List<String> scrapeSongLinks (String urlIzvođača) {
+
+        List <String> songLinks = new ArrayList<>();
+        Document doc = null;
         try {
-            doc = Jsoup.connect("https://tekstovi.net/2,1156,36784.html").get();
+            doc = Jsoup.connect(urlIzvođača).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        Elements naslovJsoup = doc.getElementsByClass("lyricCapt");
+        Elements linkovi = doc.getElementsByClass("artLyrList");
+        for (Element element : linkovi) {
+            songLinks.add(element.select("a").attr("abs:href"));
+        }
+        return songLinks;
+    }
 
+
+    // ekstrakcija teksta
+    public static void extractLyrics (String urlPjesme) {
+
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(urlPjesme).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements izvodjacNaslovJsoup = doc.getElementsByClass("lyricCapt");
+        String izvodjac = izvodjacNaslovJsoup.first().text();
+        String naslov = izvodjacNaslovJsoup.last().text();
         Elements lyricsJsoup = doc.getElementsByClass("lyric");
-
         List<String> lyrics = new ArrayList<String>();
 
         for (Element element : lyricsJsoup) {
-
             lyrics.addAll(Arrays.asList(element.toString().substring(element.toString().indexOf(">")+1).split("<br>")));
-
         }
-
         lyrics.replaceAll(String::trim);
 
         for (int i = 0; i < lyrics.size(); i++) {
@@ -44,7 +72,19 @@ public class LyricsCrawler {
             }
         }
 
-        lyrics.forEach(System.out::println);
+        Path lokacija = Paths.get("src/main/resources/" + izvodjac);
+        try {
+            Files.createDirectories(lokacija);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Files.write(Paths.get(lokacija +"/" + izvodjac + " - " + naslov + ".txt"),
+                    (Iterable<String>) lyrics.stream().map(String::valueOf)::iterator);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
